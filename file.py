b7c641c6dd1ed5107b12409ffc17e822f38d221f
Liam O'Shea <liam.oshea@avanade.com>
2022-10-19 16:31:34 +0100

    first excercise

---
+++ file.py
@@ -0,0 +1,7 @@
+english = int(input("Enter english score: "))
+maths = int(input("Enter maths score: "))
+science = int(input("Enter science score: "))
+
+average = (english + maths + science) / 3
+
+print("You avg score is: " + str(average))
